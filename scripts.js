baa0b4f26812b703b3a2630dbace08da5f125157
Julia Karolaynne <121829039+juliakarolaynne@users.noreply.github.com>
2023-08-09 21:48:53 -0300

    finalizado

--- scripts.js
+++ scripts.js
@@ -8,6 +8,18 @@ function calculateTip(event) {
         alert("Por favor, preencha os valores")
         return;
     }
+
+    if(numOfPeople == "" | numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById('each').style.display = "none"
+    } else {
+        document.getElementById('each').style.display = "block"
+    }
+
+    let total = (bill * serviceQual) / numOfPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total
+    document.getElementById('totalTip').style.display = "block";
 }
 
 document.getElementById('totalTip').style.display = "none";
